0fa52fbad91c3577e3c66a4dae532af551d106be
Ana Chan <anachan@Anas-MacBook-Pro.local>
2019-10-30 16:07:37 +0000

    added labels on durometer diagram

--- src/Components/wheelDiameter.js
+++ src/Components/wheelDiameter.js
@@ -1,64 +1,58 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import './wheelDiameter.css'
-import outlineWheel from '../Assets/Skateboard Wheel outline.svg'
+import './wheelDiameter.css';
+import outlineWheel from '../Assets/Skateboard Wheel outline.svg';
 
 
-const WheelDiameter
- = () => {
-    return ( 
-        <div className='WheelDiameter'>
-        <h2>Wheel Diameter</h2>
-    
+const WheelDiameter = () => (
+  <div className="WheelDiameter">
+    <h2>Wheel Diameter</h2>
 
-<div className='WheelDiagram-container'>
-<div>
-    <img className='outlineWheel1' src={outlineWheel} alt="skateboard wheel" />
-    <div className='wheelSize' id='48mm '>48mm</div>
-</div>
-<div>
+    <div className="WheelDiagram-container">
+      <div>
+        <img className="outlineWheel1" src={outlineWheel} alt="skateboard wheel" />
+        <div className="wheelSize" id="48mm ">48mm</div>
+      </div>
+      <div>
 
-        <img className='outlineWheel2' src={outlineWheel} alt="skateboard wheel" />
-        <div className='wheelSize' id='52mm'>52mm</div>
-</div>
-        <div>
-        <img className='outlineWheel3' src={outlineWheel} alt="skateboard wheel" />
-        <div className='wheelSize'id='56mm'>56mm</div>
-        </div>
+        <img className="outlineWheel2" src={outlineWheel} alt="skateboard wheel" />
+        <div className="wheelSize" id="52mm">52mm</div>
+      </div>
+      <div>
+        <img className="outlineWheel3" src={outlineWheel} alt="skateboard wheel" />
+        <div className="wheelSize" id="56mm">56mm</div>
+      </div>
 
-<div>
-<img className='outlineWheel4' src={outlineWheel} alt="skateboard wheel" />
-        <div className='wheelSize' id='60mm'>60mm</div>
+      <div>
+        <img className="outlineWheel4" src={outlineWheel} alt="skateboard wheel" />
+        <div className="wheelSize" id="60mm">60mm</div>
+      </div>
 
- 
-</div>
+    </div>
 
-</div>
-
-<div className='SpeedContainer'>
-<p className='label'>Less</p>
-< hr class='speedLine' ></hr>
-<p className='label'>More</p>
-{/* <p class='label'>Speed</p> */}
-</div>
+    <div className="SpeedContainer">
+      <p className="label">Less</p>
+      <hr className="speedLine" />
+      <p className="label">More</p>
+      {/* <p class='label'>Speed</p> */}
+    </div>
 
 
-<div className='AccelerationContainer'>
-<p className='label'>Quick</p>
-< hr class='accelerationLine' ></hr>
-<p className='label'>Slow</p>
-{/* <p class='label'>Acceleration</p> */}
-</div>
+    <div className="AccelerationContainer">
+      <p className="label">Quick</p>
+      <hr className="accelerationLine" />
+      <p className="label">Slow</p>
+      {/* <p class='label'>Acceleration</p> */}
+    </div>
 
 
+    <p>
 
-        <p>
-        In addition to size variations, skateboard wheels also come in different shapes, or cuts, including narrow or wide lip, and cruiser wheel shape. If you’re looking for a ride with less friction and weight, the narrow lip wheels are the way to go, and can help you master that next trick because they’re much more responsive to quick movements.
-        </p>
+In addition to size variations, skateboard wheels also come in different shapes, or cuts, including narrow or wide lip, and cruiser wheel shape. If you’re looking for a ride with less friction and weight, the narrow lip wheels are the way to go, and can help you master that next trick because they’re much more responsive to quick movements.
+    </p>
 
-        </div>
-      
-     );
-}
- 
-export default WheelDiameter
-;
+  </div>
+
+);
+
+export default WheelDiameter;
--- src/Components/wheelDurometer.js
+++ src/Components/wheelDurometer.js
@@ -1,32 +1,49 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import outlineWheel from '../Assets/Skateboard Wheel outline.svg'
-import './wheelDurometer.css'
+import outlineWheel from '../Assets/Skateboard Wheel outline.svg';
+import './wheelDurometer.css';
 
-const WheelDurometer
- = () => {
-    return ( <div>
-        <h2> Wheel Durometer</h2>
+const WheelDurometer = () => (
+  <div className="WheelDurometer">
+    <h2> Wheel Durometer</h2>
 
-<div className='wheelDurometerContainer'>
-        <img className='outlineWheelHard' src={outlineWheel} alt="skateboard wheel" />
-        <img className='outlineWheelSoft' src={outlineWheel} alt="skateboard wheel" />
+    <div className="wheelHardness">
+      <p className="soft73a">73a</p>
+      <p className="hard101a">101a</p>
+    </div>
+
+    <div className="wheelDurometerContainer">
+      <img className="outlineWheelHard" src={outlineWheel} alt="skateboard wheel" />
+
+      <div className="lineContainer">
+        <div className="softHardLineContainer">
+          <p className="label">Soft</p>
+          <hr className="softHardLine" />
+          <p className="label">Hard</p>
         </div>
+        <div className="slideLineContainer">
+          <p className="label">Less</p>
+          <hr className="slideLine" />
+          <p className="label">More</p>
+        </div>
+      </div>
 
-<p>
-Durometer measures the skateboard wheel's hardness, which can in turn specify if that particular wheel is better suited for skateboards or longboards. 
-</p>
-<ul>
-    <li>78a-87a	Soft wheel good for rough surfaces, longboards, or street boards that need lots of grip to easily roll over cracks and pebbles. Designed for smooth rides, cruising, longboards, hills, and rough surfaces.</li>
-    <li>88a-95a	Slightly harder and faster with a little less grip, but the grip's still good. Good for street and rough surfaces.</li>
-    <li>96a-99a	Nice speed and grip-- an all-around good wheel. Great for beginners skating street, skate parks, ramps, pools, and other smooth surfaces.</li>
-    <li>101a +	Hardest and fastest wheel with the least grip. Ineffective on slick and rough surfaces. These are pro wheels.</li>
-    <li>83b-84b	Wheels using the B scale are extremely hard, measuring 20 points fewer than the A Scale in order to allow the scale to extend another 20 points for the hardest wheels.</li>
+      <img className="outlineWheelSoft" src={outlineWheel} alt="skateboard wheel" />
+    </div>
 
-</ul>
+    <p>
+Durometer measures the skateboard wheel's hardness, which can in turn specify if that particular wheel is better suited for skateboards or longboards.
+    </p>
+    <ul>
+      <li>78a-87a	Soft wheel good for rough surfaces, longboards, or street boards that need lots of grip to easily roll over cracks and pebbles. Designed for smooth rides, cruising, longboards, hills, and rough surfaces.</li>
+      <li>88a-95a	Slightly harder and faster with a little less grip, but the grip's still good. Good for street and rough surfaces.</li>
+      <li>96a-99a	Nice speed and grip-- an all-around good wheel. Great for beginners skating street, skate parks, ramps, pools, and other smooth surfaces.</li>
+      <li>101a +	Hardest and fastest wheel with the least grip. Ineffective on slick and rough surfaces. These are pro wheels.</li>
+      <li>83b-84b	Wheels using the B scale are extremely hard, measuring 20 points fewer than the A Scale in order to allow the scale to extend another 20 points for the hardest wheels.</li>
+    </ul>
 
 
-    </div> );
-}
- 
-export default WheelDurometer
-;
+  </div>
+);
+
+export default WheelDurometer;
